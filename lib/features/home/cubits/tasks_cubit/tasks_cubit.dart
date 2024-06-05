52ea3de6c8ef8b2d3e7b59044be53f2c3d2fb8fe
MohamedMarzouk5 <mohamed.mz540@gmail.com>
2024-06-05 14:10:39 +0300

    task app

--- lib/features/home/cubits/tasks_cubit/tasks_cubit.dart
+++ lib/features/home/cubits/tasks_cubit/tasks_cubit.dart
@@ -9,77 +9,25 @@ import 'package:todo_app/core/constants/constants.dart';
 import 'package:todo_app/features/home/cubits/tasks_cubit/tasks_state.dart';
 import 'package:todo_app/features/home/data/models/task_model.dart';
 import 'package:todo_app/features/home/data/models/task_modl.dart';
+import 'package:todo_app/features/home/repository/home_repository.dart';
 
 class TasksCubit extends Cubit<TasksState> {
   TasksCubit() : super(TaskInitial());
-  StreamSubscription? _subscription;
+  final HomeRepository _homeRepository = HomeRepository();
   List<TaskModel>? tasks;
-  var taskBox = Hive.box<TaskModel>(kTaskBox);
-  CollectionReference tasksFirebase =
-      FirebaseFirestore.instance.collection('tasks');
   getAllTasks() {
-    tasks = taskBox.values.toList();
-
-    // print('length ${tasks!.where((task) => task.isSynced == true).toList()}');
-
-    // print('length ${tasks!.where((task) => task.isSynced == true).toList()}');
-
-    // tasks?.forEach((task) => print('task ${task.title}'));
-    // taskBox.putAt(0,TaskModel(title: title, date: date, isSynced: isSynced));
+    tasks = _homeRepository.getAllTasks();
     emit(TasksSuccess());
   }
 
-  void checkConnection() {
-    _subscription = Connectivity()
-        .onConnectivityChanged
-        .listen((ConnectivityResult result) {
-      if (result == ConnectivityResult.wifi ||
-          result == ConnectivityResult.mobile) {
-        addTaskFirebaseAndSyncInHive();
-        connected();
-      } else {
-        notConnected();
-      }
-    });
-  }
-
-  void connected() {
+  checkConnection() {
+    _homeRepository.checkConnection();
     emit(ConnectedState());
-  }
-
-  void notConnected() {
-    emit(NotConnectedState());
-  }
-
-  List<TaskModel> getUnSyncedTasks() {
-    // tasks!.where((task) => task.isSynced == false).toList().forEach(
-    //     (task) => taskBox.put(task.key, task.copyWith(isSynced: true)));
-
-    return tasks!.where((task) => task.isSynced == false).toList();
-  }
-
-  Future<void> addTaskToFirebase(TaskModel task) {
-    return tasksFirebase
-        .add({
-          'title': task.title,
-          'date': task.date,
-        })
-        .then((value) => print("User Added"))
-        .catchError((error) => print("Failed to add user: $error"));
-  }
-
-  void addTaskFirebaseAndSyncInHive() {
-    var unSyncedTasks = getUnSyncedTasks();
-    for (var task in unSyncedTasks) {
-      addTaskToFirebase(task);
-      taskBox.put(task.key, task.copyWith(isSynced: true));
-    }
-    print('done');
   }
 
   @override
   Future<void> close() {
-    _subscription!.cancel();
+    _homeRepository.cancel();
     return super.close();
   }
 }
--- lib/features/home/repository/home_repository.dart
+++ lib/features/home/repository/home_repository.dart
@@ -1,3 +1,73 @@
+import 'dart:async';
+import 'dart:developer';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:hive/hive.dart';
+import 'package:todo_app/core/constants/constants.dart';
+import 'package:todo_app/features/home/data/models/task_model.dart';
+
 class HomeRepository {
-  getAllTasks() {}
+// Firebase
+  StreamSubscription? _subscription;
+  CollectionReference tasksFirebase =
+      FirebaseFirestore.instance.collection('tasks');
+// hive
+  List<TaskModel>? _tasks;
+  Box<TaskModel> taskBox = Hive.box<TaskModel>(kTaskBox);
+
+  List<TaskModel> getAllTasks() {
+    return _tasks = taskBox.values.toList();
+  }
+
+  void checkConnection() {
+    _subscription = Connectivity()
+        .onConnectivityChanged
+        .listen((ConnectivityResult result) {
+      if (result == ConnectivityResult.wifi ||
+          result == ConnectivityResult.mobile) {
+        addTaskFirebaseAndSyncInHive();
+      }
+    });
+  }
+
+  List<TaskModel> getUnSyncedTasks() {
+    return _tasks!.where((task) => task.isSynced == false).toList();
+  }
+
+  void addTaskFirebaseAndSyncInHive() {
+    print(
+        'length false ${_tasks!.where((task) => task.isSynced == false).toList().length}');
+    var unSyncedTasks = getUnSyncedTasks();
+    for (var task in unSyncedTasks) {
+      addTaskToFirebase(task).then((function) {
+        syncInHive(task);
+      });
+    }
+    print(
+        'length true ${_tasks!.where((task) => task.isSynced == true).toList().length}');
+
+    print('done');
+  }
+
+  Future<void> addTaskToFirebase(TaskModel task) async {
+    try {
+      await tasksFirebase.add({
+        'title': task.title,
+        'date': task.date,
+      });
+      print("Task Added successfully!");
+    } catch (error) {
+      log(
+        "Failed to add task:  ${error.toString()}",
+      ); // Use console.error for logging in Flutter
+    }
+  }
+
+  void syncInHive(TaskModel task) {
+    taskBox.put(task.key, task.copyWith(isSynced: true));
+  }
+
+  cancel() {
+    _subscription?.cancel();
+  }
 }
